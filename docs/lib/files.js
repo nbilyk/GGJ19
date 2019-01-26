a408fbbc2a8d812e5b6fcc7746c45ef73b538c0d
Nick <nick@polyforest.com>
2019-01-26 13:59:09 -0600

    pinch zoom

--- docs/lib/files.js
+++ docs/lib/files.js
@@ -2,61 +2,61 @@ var manifest = {
 	"files": [
 		{
 			"path": "lib/AcornGame.js",
-			"modified": 1548532197940,
+			"modified": 1548532601650,
 			"size": 12946,
 			"mimeType": null
 		},
 		{
 			"path": "lib/AcornSpine.js",
-			"modified": 1548532197942,
+			"modified": 1548532601652,
 			"size": 7073,
 			"mimeType": null
 		},
 		{
 			"path": "lib/AcornUiCore.js",
-			"modified": 1548532197976,
+			"modified": 1548532601686,
 			"size": 1395842,
 			"mimeType": null
 		},
 		{
 			"path": "lib/AcornUiJsBackend.js",
-			"modified": 1548532197988,
+			"modified": 1548532601695,
 			"size": 288660,
 			"mimeType": null
 		},
 		{
 			"path": "lib/AcornUtils.js",
-			"modified": 1548532197995,
+			"modified": 1548532601703,
 			"size": 459219,
 			"mimeType": null
 		},
 		{
 			"path": "lib/GGJ19Core.js",
-			"modified": 1548532197998,
+			"modified": 1548532601709,
 			"size": 41858,
 			"mimeType": null
 		},
 		{
 			"path": "lib/GGJ19Js.js",
-			"modified": 1548532198002,
+			"modified": 1548532601712,
 			"size": 2227,
 			"mimeType": null
 		},
 		{
 			"path": "lib/gGJ19.js",
-			"modified": 1548532197996,
+			"modified": 1548532601705,
 			"size": 913,
 			"mimeType": null
 		},
 		{
 			"path": "lib/kotlin.js",
-			"modified": 1548532198014,
+			"modified": 1548532601726,
 			"size": 346450,
 			"mimeType": null
 		},
 		{
 			"path": "lib/require.js",
-			"modified": 1548532198016,
+			"modified": 1548532601738,
 			"size": 17869,
 			"mimeType": null
 		}
